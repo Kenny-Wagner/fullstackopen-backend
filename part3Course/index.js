c94fa8ad467fb316239889745eabc66034478349
Kenny-Wagner <47788231+Kenny-Wagner@users.noreply.github.com>
2024-05-01 18:21:53 +0000

    Added env port

--- part3Course/index.js
+++ part3Course/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 let notes = [
@@ -91,6 +93,6 @@ const unkownEndpoint = (request, response) => {
 }
 app.use(unkownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
